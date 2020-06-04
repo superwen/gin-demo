6caff570c8f058966556f24f45803e7962d46013
chenshengwen <chenshengwen@huan.tv>
2020-06-04 10:35:35 +0800

    add web server

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+
+	r.StaticFile("/favicon.ico", "./favicon.ico")
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "hello gin!",
+		})
+	})
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
